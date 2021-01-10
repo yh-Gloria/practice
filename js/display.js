2f526d13b4c1d28907b24923b8597f21179d2ca3
yh <1320395462@qq.com>
2021-01-10 18:12:35 +0800

    calculator v1.0

---
+++ js/display.js
@@ -0,0 +1,31 @@
+
+			let var1_count = 0;
+			let var2_count = 0;
+			let operator_flag = 0;
+			let keys_value1 = 0;
+			function keys_operation( keys )
+			{
+				if(keys.id == 'number' && operator_flag == 0)
+				{
+					keys_value1 = keys_value1 | parseInt(keys.value) << var1_count;
+					var1_count++;
+				    if();
+				}
+
+				
+			// if()
+			// {
+			// 	l
+			//     document.getElementById('display').setAttribute('value', keys_value1);
+			// }
+			// if(count == 3)
+			// {
+			// 	let keys_value2 = keys.getAttribute("value");
+			//     document.getElementById('display').setAttribute('value', keys_value2);
+			// }
+			// if( keys.id == "operator" )
+			// {
+			// 	console.log(keys_value1 + keys_value2);
+
+			// }
+		 }
